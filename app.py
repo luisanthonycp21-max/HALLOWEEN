3925a9c62619dccbeef43c93869f91df960c1dcb
luisanthonycp21-max <luisanthonycp.21@gmail.com>
2026-09-01 14:15:09 -0500

    app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -11,8 +12,5 @@ def home():
     return render_template("index.html", nombre=nombre_invitado)
 
 if __name__ == "__main__":
-    app.run(import os
-
-if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5001))
-    app.run(host='0.0.0.0', port=port))  # <--- Aquí cambiamos al puerto 5001
+    app.run(host='0.0.0.0', port=port)
